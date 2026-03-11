27e9b12a870d6a8cc54d30ff047d2b56da84e4bb
thevun356 <thevun.20211356@iit.ac.lk>
2026-03-11 10:04:50 +0530

    Base proeject setup

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "@babel/preset-typescript",
+      "nativewind/babel",
+    ],
+    plugins: ["react-native-reanimated/plugin"],
+  };
+};
